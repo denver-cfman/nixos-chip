1d4fdc6520a14e25e6fbd9925a4179cb7e2a8002
Mr Z <denver.cfman@gmail.com>
2026-06-18 13:48:37 -0600

    Refactor flake.nix for ARM cross-compilation

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-# flake.nix
 {
   description = "NixOS for NextThing Co. C.H.I.P.";
 
@@ -6,33 +5,49 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.chip = nixpkgs.lib.nixosSystem {
-      # Target cross-compilation to ARMv7l
-      modules = [
-        ({ config, pkgs, ... }: {
-          nixpkgs.localSystem.system = "x86_64-linux"; # Your host system
-          nixpkgs.crossSystem.system = "armv7l-linux";
+  outputs = { self, nixpkgs }: 
+    let
+      system = "x86_64-linux"; # Your host development machine
+    in {
+      packages.${system}.default = self.nixosConfigurations.chip.config.system.build.tarball;
 
-          boot.kernelPackages = pkgs.linuxPackages_latest;
-          boot.supportedFilesystems = [ "ext4" "ubifs" ];
-          
-          # Target Allwinner R8/A13 configuration
-          boot.loader.generic-extlinux-compatible.enable = true;
-          
-          # Include necessary firmware for the RTL8723BS Wi-Fi / Bluetooth chip
-          hardware.enableRedistributableFirmware = true;
+      nixosConfigurations.chip = nixpkgs.lib.nixosSystem {
+        modules = [
+          ({ config, pkgs, ... }: {
+            nixpkgs.localSystem.system = system;
+            nixpkgs.crossSystem.system = "armv7l-linux";
 
-          # Headless serial console setup
-          boot.kernelParams = [ "console=ttyS0,115200n8" "earlyprintk" ];
+            # Direct generation of the root filesystem tarball layout
+            system.build.tarball = pkgs.callPackage "${nixpkgs}/nixos/lib/make-system-tarball.nix" {
+              fileName = "nixos-chip-rootfs";
+              compressCommand = "${pkgs.gzip}/bin/gzip -c"; 
+              storeContents = [ { object = config.system.build.toplevel; symlink = "none"; } ];
+              contents = [
+                { source = config.system.build.toplevel + "/init"; target = "/sbin/init"; }
+              ];
+            };
 
-          networking.hostName = "ntc-chip";
-          
-          # Configure basic user and SSH access
-          services.openssh.enable = true;
-          users.users.root.initialPassword = "nixos";
-        })
-      ];
+            # FIX: Satisfy the fileSystems safety check by defining a root mount placeholder
+            fileSystems."/" = {
+              device = "/dev/nand"; # Placeholder for C.H.I.P. raw NAND/UBI mount points
+              fsType = "ubifs";     # Matches the C.H.I.P. flash infrastructure
+            };
+
+            # FIX: Explicitly disable GRUB to satisfy the bootloader configuration safety check
+            boot.loader.grub.enable = false;
+            boot.loader.generic-extlinux-compatible.enable = true;
+
+            boot.kernelPackages = pkgs.linuxPackages_latest;
+            boot.supportedFilesystems = [ "ext4" "ubifs" ];
+            
+            hardware.enableRedistributableFirmware = true;
+            boot.kernelParams = [ "console=ttyS0,115200n8" "earlyprintk" ];
+
+            networking.hostName = "ntc-chip";
+            services.openssh.enable = true;
+            users.users.root.initialPassword = "nixos";
+          })
+        ];
+      };
     };
-  };
 }
