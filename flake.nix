5511b02f631b66437253180735bfd0eafe60ded5
Mr Z <denver.cfman@gmail.com>
2026-06-19 15:52:22 -0600

    Refactor flake.nix for improved structure and clarity

--- flake.nix
+++ flake.nix
@@ -5,101 +5,44 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: 
-    let
-      system = "x86_64-linux"; # Your host development machine
-    in {
-      packages.${system}.default = self.nixosConfigurations.chip.config.system.build.tarball;
+  outputs = { self, nixpkgs }: {
 
-      nixosConfigurations.chip = nixpkgs.lib.nixosSystem {
-        modules = [
-          ({ config, pkgs, ... }: {
-            nixpkgs.localSystem.system = system;
-            nixpkgs.crossSystem.system = "armv7l-linux";
-
-            # Direct generation of the root filesystem tarball layout
-            system.build.tarball = pkgs.callPackage "${nixpkgs}/nixos/lib/make-system-tarball.nix" {
-              fileName = "nixos-chip-rootfs";
-              compressCommand = "${pkgs.gzip}/bin/gzip -c"; 
-              storeContents = [ { object = config.system.build.toplevel; symlink = "none"; } ];
-              contents = [
-                { source = config.system.build.toplevel + "/init"; target = "/sbin/init"; }
-              ];
-            };
-
-            # FIX: Satisfy the fileSystems safety check by defining a root mount placeholder
-            fileSystems."/" = {
-              device = "/dev/nand"; # Placeholder for C.H.I.P. raw NAND/UBI mount points
-              fsType = "ubifs";     # Matches the C.H.I.P. flash infrastructure
-            };
-
-            # FIX: Explicitly disable GRUB to satisfy the bootloader configuration safety check
-            boot.loader.grub.enable = false;
-            boot.loader.generic-extlinux-compatible.enable = true;
-
-            boot.kernelPackages = pkgs.linuxPackages_latest;
-            boot.supportedFilesystems = [ "ext4" "ubifs" ];
-            
-            hardware.enableRedistributableFirmware = true;
-            boot.kernelParams = [ "console=ttyS0,115200n8" "earlyprintk" ];
-
-            networking.hostName = "ntc-chip";
-
-
-            time.timeZone = "America/Denver";
-          
-            i18n.defaultLocale = "en_US.UTF-8";
-          
-            i18n.extraLocaleSettings = {
-              LC_ADDRESS = "en_US.UTF-8";
-              LC_IDENTIFICATION = "en_US.UTF-8";
-              LC_MEASUREMENT = "en_US.UTF-8";
-              LC_MONETARY = "en_US.UTF-8";
-              LC_NAME = "en_US.UTF-8";
-              LC_NUMERIC = "en_US.UTF-8";
-              LC_PAPER = "en_US.UTF-8";
-              LC_TELEPHONE = "en_US.UTF-8";
-              LC_TIME = "en_US.UTF-8";
-            };
-          
-            users.users.giezac = {
-              isNormalUser = true;
-              description = "giezac";
-              extraGroups = [ "networkmanager" "wheel" ];
-              packages = with pkgs; [
-                oh-my-zsh 
-              ];
-              password = "changeme";
-              openssh.authorizedKeys.keys = [
-                  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCZawwmpdesq0ZvtXTdPekpjK3OYiPONrKO0no625FqYG8A8fZY++cxjG4my6HgmoaBrZiWvRJTa0WfTfw9Tzx9xt/FKrCB4bk9G33WP+RJNF7AEo3wkGGBLHzxp9bnhzzxdJOQCV67DRDxQNjMiR5S/bkSU+QYPDq+MLLx8mFz8lfzOSThVgDLjOj7lsRAJcrFDawsjZYHjsVBdDfCkjXGPKT7/c90k0BOvOjnOZ4vEn1w2s/Neq0rDTJYDUSmu9SzW/+WkM1rZa4GS5QGFMJVrI1Ow3X8tiUYpAp1oa0MyIpRkpuP39W+I6qaRBW4/+lyJYWsLP09hU7K2wT6OGap forGitHub"
-              ];
-            };
-          
-            users.users.root.openssh.authorizedKeys.keys = [
-                  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPmNXnRi9A/6hQL0wxpyti2Qo+Sd8LZt0uLu/hSJ91tH root@R210ii"
-            ];
-
-            nixpkgs.config.allowUnfree = true;
-          
-            nix.settings.experimental-features = [ "nix-command" "flakes" ];
-          
-            environment.systemPackages = with pkgs; [
-              vim
-              wget
-              htop
-              btop
-              iftop
-              curl
-              git
-              fastfetch
-              jq
-              screen
-            ];
-
-            services.openssh.enable = true;
-            users.users.root.initialPassword = "nixos";
-          })
-        ];
-      };
+    # 1. Target configuration for LIVE maintenance (nixos-rebuild)
+    # This evaluates natively when run on the device.
+    nixosConfigurations.chip = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix # Inline logic moved here for readability, or keep inline
+        ({ ... }: {
+          # On-device native building
+          nixpkgs.hostPlatform = "armv7l-linux";
+        })
+      ];
     };
+
+    # 2. Cross-compiled configuration specifically for host-driven tarball builds
+    # This forces the build matrix to handle the x86_64 -> armv7l cross-compilation pipeline.
+    packages.x86_64-linux.default = 
+      let
+        crossPkgs = nixpkgs.lib.nixosSystem {
+          modules = [
+            ./configuration.nix
+            ({ config, pkgs, ... }: {
+              nixpkgs.localSystem.system = "x86_64-linux";
+              nixpkgs.crossSystem.system = "armv7l-linux";
+
+              # Direct generation of the root filesystem tarball layout
+              system.build.tarball = pkgs.callPackage "${nixpkgs}/nixos/lib/make-system-tarball.nix" {
+                fileName = "nixos-chip-rootfs";
+                compressCommand = "${pkgs.gzip}/bin/gzip -c"; 
+                storeContents = [ { object = config.system.build.toplevel; symlink = "none"; } ];
+                contents = [
+                  { source = config.system.build.toplevel + "/init"; target = "/sbin/init"; }
+                ];
+              };
+            })
+          ];
+        };
+      in
+        crossPkgs.config.system.build.tarball;
+  };
 }
